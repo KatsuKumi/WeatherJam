2a8b10c261cbe7c89e86566be424a298d36883b3
youngdreamer33 <b92.a75@gmail.com>
2017-03-30 10:45:45 +0200

    Base HTML

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <title>WeatherJam: </title>
+</head>
+
+<body>
+<form>
+    <div class="form-group">
+        <label for="weather"> Tell us where you are </label>
+        <input type="city" class="form-control" id="city" value="" placeholder="Enter a city name.">
+    </div>
+    <button type="submit" id="sendbtn" class="btn btn-default">Submit</button>
+
+</form>
+
+<div id='affichage'></div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="meteo.js"></script>
+
+<div id="cityweather"></div>
+
 <?php
 
 ?>
+</body>
+
+</html>
