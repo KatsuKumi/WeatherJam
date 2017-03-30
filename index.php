903f63f4ef7ad89bccb3737ec33a126e14cf692b
Alex-san <bobbax4@live.fr>
2017-03-30 21:36:28 +0200

    Ajout De la gestion de la météo, c'était long. Lel

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php include('weatherimgtable.php')?>
 <html lang="en">
 
 <head>
@@ -6,11 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css"><link href="css/owfont-regular.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="style.css">
     <link href="css/owfont-regular.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-
     <link href="css/weather-icons-wind.min.css" rel="stylesheet" type="text/css">
     <link href="css/weather-icons.css" rel="stylesheet" type="text/css">
     <title>WeatherJam: the sound that fits your mood </title>
@@ -47,22 +47,75 @@
 </div>
 
 
-<div id='affichage' class="col-md-offset-4 col-md-4 col-xs-offset-2 col-xs-8 panel panel-default ">
+<div id='affichage' class="col-md-offset-2 col-md-8 col-xs-offset-2 col-xs-8 panel panel-default container">
 
     <?php
     $city = htmlspecialchars($_POST["city"]);
     if (!empty($city)){
         $weatherjson = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$city."&units=metric&appid=0e6f23f3b33300d4b282e44cd350003e");
         $weatherarray = json_decode($weatherjson, true);
+        /*var_dump($weatherarray);*/
         $pressure = $weatherarray["main"]["pressure"]* 0.00001 *100;
         $temp = $weatherarray["main"]["temp"];
         $humidity = $weatherarray["main"]["humidity"];
         $winddegree = $weatherarray["wind"]["deg"];
+        $windspeed = $weatherarray["wind"]["speed"];
         $iconweather = $weatherarray['weather'][0]['id'];
-        echo '<strong>'.$city.'</strong><br>';
-        echo '<i class="owf owf-'.$iconweather.' owf-5x"></i>';
-        echo '<i class="wi wi-wind towards-'.$winddegree.'-deg owf-5x"></i>';
-        echo '<i class="wi wi-day-sunny owf-5x""></i>';
+        $weather = $weatherarray['weather'][0]['description'];
+        $weatherjsontom = file_get_contents("http://api.openweathermap.org/data/2.5/forecast?q=".$city."&appid=0e6f23f3b33300d4b282e44cd350003e");
+        $weatherarraytom = json_decode($weatherjsontom, true)["list"][8];
+        $weathertom = $weatherarraytom['weather'][0]['description'];
+        echo "
+    <div class=\"row weathercontent\">
+        <div class=\"col-md-3 col-xs-12\"><br>
+            <div id=\"centeredleft\">
+                <div class=\"weatherico\">
+                    <i class=\"wi ".$weatherimg[$weather]." owf-5x\"></i>
+
+                </div>
+
+                <div class=\"titleleft\">
+                    <span>".$city."</span><br>
+                    <span>".ucfirst($weather)."</span>
+                </div><br>
+            </div>
+        </div>
+        <div id=\"right\" class=\"col-md-5 col-xs-12\">
+            <div id=\"stats\">
+                <i class=\"wi wi-thermometer owf-3x\"></i><span>".$temp."</span><i class=\"wi wi-celsius owf-3x\"></i><br>
+                <i class=\"wi wi-humidity owf-3x\"></i><span>".$humidity." %</span><br>
+                <i class=\"wi wi-barometer owf-3x\"></i><span>".$pressure." Bar</span><br>
+            </div>
+
+        </div>
+        <div class=\"col-lg-3 col-md-12\">
+
+            <i class=\"wi wi-wind towards-".$winddegree."-deg owf-5x\"></i>
+            <span class=\"degreetitle\">".$winddegree."</span><i class=\"wi wi-degrees owf-4x\"></i><br>
+            <i class=\"wi wi-wind-beaufort-".round($windspeed, 0)." owf-5x\"></i>
+        </div>
+    </div>
+    <hr>
+    <div class=\"row\">
+        <div class=\"col-lg-12 col-md-12 titleweather\" >
+            Tomorrow
+        </div>
+    </div>
+    <div class=\"row weathertommorow\">
+        <div class=\"col-lg-12 col-md-12\" >
+            <div id=\"centeredleft\">
+                <div class=\"weatherico\">
+                    <i class=\"wi ".$weatherimg[$weathertom]." owf-5x\"></i>
+
+                </div>
+
+                <div class=\"titleleft\">
+                    <span>".ucfirst($weathertom)."</span>
+                </div><br>
+            </div>
+
+        </div>
+    </div>";
     }
     ?>
 </div>
---
+++ weatherimgtable.php
@@ -0,0 +1,17 @@
+<?php
+
+$weatherimg["clear sky"] = "wi-day-sunny";
+$weatherimg["few clouds"] = "wi-day-cloudy";
+$weatherimg["scattered clouds"] = "wi-cloudy";
+$weatherimg["broken clouds"] = "wi-day-fog";
+$weatherimg["shower rain"] = "wi-showers";
+$weatherimg["rain"] = "wi-rain";
+$weatherimg["light rain"] = "wi-rain";
+$weatherimg["thunderstorm"] = "wi-thunderstorm";
+$weatherimg["snow"] = "wi-snowflake-cold";
+$weatherimg["mist"] = "wi-windy";
+
+
+$keywordweather["clear sky"] = ["happy", "smile", "enjoy", "love", "friendly"];
+
+?>
